ebc4950a235efd06cafc560019220778d7d15100
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-08-13 15:15:25 +0300

    QuestionPractical

--- AssignmentOPP_Session4/Program.cs
+++ AssignmentOPP_Session4/Program.cs
@@ -1,10 +1,76 @@
-﻿namespace AssignmentOPP_Session4
+﻿using AssignmentOPP_Session4;
+using System.Reflection.Metadata;
+
+namespace Assignment_Session03OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            StandardShipment standard = new StandardShipment("SH001", "Laptop", 3, 80);
+            ExpressShipment Express = new ExpressShipment("SH002", "Mobile Phone", 2, 60, 30);
+            InternationalShipment international = new InternationalShipment("SH003", "Televition", 8, 120, "Germany", 100);
+
+            DeliveryCenter DC = new DeliveryCenter();
+            Console.WriteLine(DC.AddShipment(standard) ? "Shipment Added Succssfully" : "Shipment Not Added");
+            Console.WriteLine(DC.AddShipment(Express) ? "Shipment Added Succssfully" : "Shipment Not Added");
+            Console.WriteLine(DC.AddShipment(international) ? "Shipment Added Succssfully" : "Shipment Not Added");
+
+            Console.WriteLine("\n=======================================\n");
+
+            Console.WriteLine("Standard Shipment\n");
+            DeliveryHelper.PrintShipmentDetails(standard);
+            Console.WriteLine("\n=======================================\n");
+
+            Console.WriteLine("Express Shipment\n");
+            DeliveryHelper.PrintShipmentDetails(Express);
+            Console.WriteLine("\n=======================================\n");
+
+            Console.WriteLine("International Shipment\n");
+            DeliveryHelper.PrintShipmentDetails(international);
+            Console.WriteLine("\n=======================================\n");
+
+            Console.WriteLine("Tracking Status with interface array");
+
+            ITrackable[] Track = { standard, Express, international };
+
+            foreach(ITrackable x in Track)
+            {
+                Console.WriteLine(x.GetTrackingStatus());
+            }
+            
+            IInsurable[] Insurable = { standard, Express, international };
+            Console.WriteLine("\n=======================================\n");
+
+            foreach (IInsurable x in Insurable)
+            {
+                if (x is StandardShipment )
+                    Console.WriteLine($"Standard Shipment Insurance : {x.CalculateInsurance()} EGP");
+
+                if (x is ExpressShipment )
+                    Console.WriteLine($"Express Shipment Insurance : {x.CalculateInsurance()} EGP");
+
+                if (x is InternationalShipment)
+                    Console.WriteLine($"International Shipment Insurance : {x.CalculateInsurance()} EGP");
+            }
+            Console.WriteLine("\n=======================================\n");
+            Console.WriteLine("\nTracking Status with DeliveryReport \n");
+
+            DeliveryReport report = new DeliveryReport();
+            report.PrintShipment(standard);
+            report.PrintShipment(Express);
+            report.PrintShipment(international);
+            Console.WriteLine("\n=======================================\n");
+
+            Console.WriteLine("\nPrint Insurance Status with DeliveryReport \n");
+            Console.Write("Standard Shipment Insurance : "); report.PrintInsurance(standard); Console.Write(" EGP\n");
+            Console.Write("Express Shipment Insurance : "); report.PrintInsurance(Express); Console.Write(" EGP\n");
+            Console.Write("International Shipment Insurance : "); report.PrintInsurance(international); Console.Write(" EGP\n");
+
+
+            Console.WriteLine("\n=======================================\n");
+            Console.WriteLine("Interface Polymorphism Demonstrated Successfully.");
         }
-    }
+}
 }
---
+++ AssignmentOPP_Session4/StandardShipment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOPP_Session4
+{
+    public class StandardShipment:Shipment,ITrackable,IInsurable
+    {
+        public StandardShipment()
+        {
+            
+        }
+        public StandardShipment(string? _trackingCode, string _description, decimal _weight, decimal _deliveryFee) : base(_trackingCode, _description, _weight, _deliveryFee)
+        {
+
+        }
+        public override decimal EstimatedCost
+        { 
+            get => DeliveryFee + (Weight* 5);      
+        }
+       public string GetTrackingStatus()
+        {
+            return $"Shipment SH001 is Ready.";
+        }
+        public decimal CalculateInsurance()
+        {
+            return EstimatedCost * 0.05m;
+        }
+        public override void PrintShipment()
+        {
+            Console.WriteLine($" trackingCode : {TrackingCode}\n " +
+                $"description  : {Description} \n " +
+                $" weight : {Weight}\n    deliveryFee : {DeliveryFee}\n EstimatedCost : {EstimatedCost} "); 
+        }
+    }
+}
